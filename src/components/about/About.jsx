83b04c027ce61b39465fe9ff2801319e90e22a36
Krishna Nand Patel <imkrishna8737@gmail.com>
2023-04-26 20:45:53 +0530

    work done by Ashish
    Signed-off-by: Krishna Nand Patel <imkrishna8737@gmail.com>

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,240 @@
-import React from 'react'
+import React from "react";
+import "../styles/About.css";
 
 function About() {
   return (
     <div>
-      About
+      <div className="AboutBox">
+        <div className="mainBox">
+          <div className="mainUpperBox">
+            <div className="mainLeftBox">
+              I'm a Product & Interaction Designer based out of Bengaluru and
+              currently working at Publicis Sapient.
+            </div>
+            <div className="mainRightBox"></div>
+          </div>
+          <div className="mainLowerBox">
+            <p>
+              Hey there!
+              <span className="whiteFontColor">
+                &nbsp; I'm Kshitij Srivastava and I'm a UX designer currently
+                living in Bengaluru.
+              </span>
+              But let me take you back to the beginning of my journey.
+            </p>
+            <br />
+            <p>
+              I was born and raised in the city of Kanpur, a bustling city in
+              northern India. As a child, I always loved art and design, and
+              spent many hours drawing and creating my own little sketches. When
+              it was time for me to attend college, I decided to pursue my
+              passion and study design at a college in Pune.
+            </p>
+            <br />
+            <p>
+              <span className="whiteFontColor">During my time in Pune,</span> I
+              learned a lot about various aspects of design, from graphic design
+              to product design. However, it was during a course on user
+              experience design that I found my true calling. I loved the idea
+              of designing products and experiences that not only looked
+              beautiful, but also worked seamlessly for users.
+            </p>
+            <br />
+            <p>
+              After college, I landed my first job in
+              <span className="whiteFontColor">
+                &nbsp; Mumbai as a junior designer
+              </span>
+              at a start-up. It was an exciting and challenging experience,as I
+              worked on a variety of projects and had the opportunity to learn
+              from more experienced designers. But, as time went on,I began to
+              feel the itch for something new and exciting.
+            </p>
+            <br />
+            <p>
+              That's when I packed my bags and
+              <span className="whiteFontColor">
+                &nbsp; headed to Bengaluru, the "Silicon Valley" of India.
+              </span>
+              I was ready to take on new challenges and learn as much as I
+              could. Here, I worked for several years as a senior UX designer at
+              a well-known tech company. I loved the fast-paced, innovative
+              culture of Bengaluru, but I also felt that there was something
+              missing.
+            </p>
+            <br />
+            <p>
+              And then, came the opportunity of a lifetime a chance to
+              <span className="whiteFontColor">
+                &nbsp; travel to South Africa for work.
+              </span>
+              I was thrilled at the prospect of working as a UX designer in a
+              new environment, and using my skills to create meaningful
+              experiences for people from all over the world. It was a
+              life-changing experience and made me realize that this was where I
+              wanted to settle down and make a home.
+            </p>
+            <br />
+            <p>
+              But outside of work, I also have a passion for
+              <span className="whiteFontColor">
+                {" "}
+                &nbsp; digital paintings{" "}
+              </span>{" "}
+              and capturing abandonment places taken over by nature, It was my
+              way of exploring my creative side and appreciating the beauty of
+              the world around us
+            </p>
+            <br />
+            <p>
+              Below you can find a comprehensive overview of my professional
+              experience as a designer
+            </p>
+            <br />
+          </div>
+        </div>
+        <hr className="hrTag" />
+        <div className="ExpMainBox">
+          <div className="expNav">
+            <div className="expNavLeft">
+              <div className="expnavTitle">Experience</div>
+              <hr className="expnavTitlehr" />
+            </div>
+            <div className="expNavRight">
+              <div className="expNavdownloadIcon"></div>
+              <div className="expNavdownloadText">Download Resume</div>
+            </div>
+          </div>
+          <div className="expContentOne">
+            <div className="expLeftContent">
+              <p className="expLeftText">2021 - Current</p>
+              <p className="expLeftTextName">Senior UX Designer</p>
+              <p className="expLeftText">Publicis Sapient - Bengaluru,IN</p>
+            </div>
+            <div className="expRightContent">
+              I created an enhanced experience for Verizon's C recommendations,
+              and documenting use cases and user stories. I worked closely with
+              stakeholders and engineers to troubleshoot UX problems and improve
+              user engagement. <br /> <br />I designed the user experience for
+              PGOS, a campaign management application, with the aim of providing
+              a more intuitive and efficient platform for marketing teams to
+              manage their campaigns effectively.
+            </div>
+          </div>
+          <div className="expContentTwo">
+            <div className="expLeftContent">
+              <p className="expLeftText">2018 - 2021</p>
+              <p className="expLeftTextName">Design Lead</p>
+              <p className="expLeftText">
+                Moonraft Innovation Labs - <br /> Bengaluru,IN
+              </p>
+            </div>
+            <div className="expRightContent">
+              I gathered and evaluated user requirements for Subex,
+              collaborating with product managers and engineers to create
+              wireframes, prototypes, mock-ups, and detailed interactions.{" "}
+              <br />
+              <br /> I was leading the complete user experience redesign and
+              user interface for Standard Bank South Africa's web portal, which
+              was using a legacy product that required a revamp. <br /> <br />
+              For Dell Find a Partner, I gather and evaluate user requirements,
+              create wireframes, prototypes, and visual designs.
+            </div>
+          </div>
+          <div className="expContentThree">
+            <div className="expLeftContentThree">
+              <p className="expLeftText">2017 - 2018</p>
+              <p className="expLeftTextName">Product Designer</p>
+              <p className="expLeftText">Moneytap - Bengaluru,IN</p>
+            </div>
+            <div className="expRightContent">
+              I collaborated with the in-house credit team and developers to
+              design version 3.0 of the Banker's Portal, improving its
+              usability.
+              <br />
+              <br /> I also optimised Moneytap' website, reducing its loading
+              time and enhancing the understanding of its product offerings,
+              which increased the visit-to-conversion ratio.
+            </div>
+          </div>
+          <div className="expContentFour">
+            <div className="expLeftContent">
+              <p className="expLeftText">2014 - 2017</p>
+              <p className="expLeftTextName">Hybrid Designer</p>
+              <p className="expLeftText">The Creon Studio - Mumbai,IN</p>
+            </div>
+            <div className="expRightContent">
+              I was responsible for creating architecture, navigation, personas,
+              user journeys, and wireframe sketches. I collaborated with the
+              development team to provide UX guidelines, Ul patterns, and
+              actionable recommendations.
+              <br />
+              <br /> Additionally, I mentored graduate trainees on user
+              experience design fundamentals.
+              <br /> <br />
+              Some of the clients I worked with were Schoolkart, First Crayon,
+              ET Sales, ET Startup Awards 2016, Sirius Sport, and 9xm.
+            </div>
+          </div>
+          <div className="expContentFive">
+            <div className="expLeftContent">
+              <p className="expLeftText">2013 - 2014</p>
+              <p className="expLeftTextName">Designer</p>
+              <p className="expLeftText">HB Design - Mumbai,IN</p>
+            </div>
+            <div className="expRightContent">
+              As a web and graphic designer, my responsibilities included
+              designing websites and emailers for Accenture, TCS and WNS. This
+              involved collaborating with stakeholders to understand their needs
+              and goals, and then translating those into visually appealing and
+              user-friendly designs.
+              <br />
+              <br /> Additionally, I created marketing materials such as flyers,
+              ads, and other graphics for TCS, WNS, and Box Office India.
+            </div>
+          </div>
+          <div className="expContentSix">
+            <div className="expLeftContent">
+              <p className="expLeftText">2012 - 2013</p>
+              <p className="expLeftTextName">Junior Designer</p>
+              <p className="expLeftText">Hulahoop Design - Mumbai,IN</p>
+            </div>
+            <div className="expRightContent">
+              I was responsible for designing a signage system to modernise the
+              branded marketing and aesthetics for JW Marriott. This included a
+              new color scheme and typography to improve the visual appeal of
+              the signage system.
+              <br />
+              <br />
+              Additionally, I created various printed materials for Raheja's
+              Pebble Bay, which includes print and banner ads, newspaper ads,
+              flyers, and handouts.
+            </div>
+          </div>
+        </div>
+        <div className="EducMainBox">
+          <div className="expnavTitle">Education</div>
+          <hr className="expnavTitlehr" />
+          <div className="expContentSix">
+            <div className="EduLeftContent">
+              <p className="expLeftText">2008 - 2012</p>
+              <p className="expLeftTextName">
+                Bachelor of Fine Arts <br /> (Appkied Arts)
+              </p>
+              <p className="expLeftText">University of Pune,IN</p>
+            </div>
+            <div className="EduRightContent">
+              I specialised in Visual Communication. During my course, I studied
+              various subjects related to art and design, such as typography,
+              graphic design, illustration, and branding, which equipped me with
+              the skills to create visually appealing designs that prioritise
+              user needs and enhance their overall experience.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
